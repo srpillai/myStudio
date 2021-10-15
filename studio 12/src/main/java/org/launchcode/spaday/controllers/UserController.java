73b449d66815575d5970a3fcd0fa3c57022da88b
srpillai <srpillai@gmail.com>
2021-10-14 21:30:02 -0500

    add exercise 11

---
+++ studio 12/src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,34 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
+        return "user/add";
+    }
+
+    @PostMapping("/add")
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+
+        if (errors.hasErrors() || !user.getPassword().equals(verify)) {
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+        return "user/index";
+
+
+    }
+
+
+}
